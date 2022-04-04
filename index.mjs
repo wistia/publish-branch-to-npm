5a21d85e2b71dc2a3625f0bc0c8cb5dcda03a6e1
Morgan Wigmanich <mwigmanich@wistia.com>
2022-04-04 19:15:58 -0400

    move postCommentToPullRequest & getCommentList functions to helpers file

--- index.mjs
+++ index.mjs
@@ -4,63 +4,24 @@ import { context } from '@actions/github';
 
 import {
   coerceToBoolean,
-  getUniqueVersion,
   generatePullRequestComment,
   getCommentIdentifier,
-  getCommentId,
   getGithubClient,
   getNpmAuthCommand,
-  getUpdatePackageVersionCommand,
   getPackageNameAndVersion,
-  loadPackageJson,
   getPublishPackageCommand,
+  getUniqueVersion,
+  getUpdatePackageVersionCommand,
+  loadPackageJson,
+  postCommentToPullRequest,
 } from './helpers.mjs';
 
-// returns an array of comments from current PR
-const getCommentList = async (client, issue) => {
-  const options = {
-    owner: issue.owner,
-    repo: issue.repo,
-    issue_number: issue.number,
-  };
-
-  return client.rest.issues.listComments(options);
-};
-
-// posts a comment to the PR if none have been posted yet, but any new posts
-// (for example, when a new commit is pushed to the PR) will update original comment
-// so that there is only ever a single comment being made by this action
-const postCommentToPullRequest = async (client, commentBody) => {
-  if (!context.issue.number) {
-    throw new Error('This is not a PR or commenting is disabled');
-  }
-
-  const { data: commentList } = await getCommentList(client, context.issue);
-  const commentId = getCommentId(commentList, getCommentIdentifier());
-
-  if (commentId) {
-    await client.rest.issues.updateComment({
-      issue_number: context.issue.number,
-      owner: context.issue.owner,
-      repo: context.issue.repo,
-      body: commentBody,
-      comment_id: commentId,
-    });
-    return;
-  }
-
-  await client.rest.issues.createComment({
-    issue_number: context.issue.number,
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    body: commentBody,
-  });
-};
-
 try {
+  // action inputs
   const githubToken = core.getInput('github_token');
   const npmToken = core.getInput('npm_token');
   const commitHash = context.payload.after;
+  const isDryRun = core.getInput('dry_run');
 
   // early exit because we cannot proceed without these variables
   if (!githubToken) {
@@ -72,10 +33,9 @@ try {
   }
 
   if (!commitHash) {
-    throw new Error('Current commit could not be determined');
+    throw new Error('Current commit hash could not be determined');
   }
 
-  const isDryRun = coerceToBoolean(core.getInput('dry_run'));
   const githubClient = getGithubClient(githubToken);
   const { name, currentVersion } = loadPackageJson();
   const uniqueVersion = getUniqueVersion(currentVersion, commitHash);
@@ -91,10 +51,10 @@ try {
   execSync(getUpdatePackageVersionCommand(uniqueVersion));
 
   // publish package
-  execSync(getPublishPackageCommand(isDryRun));
+  execSync(getPublishPackageCommand(coerceToBoolean(isDryRun)));
 
   // post comment to PR
-  await postCommentToPullRequest(githubClient, commentBody);
+  await postCommentToPullRequest(context, githubClient, commentBody);
 } catch (error) {
   console.log(error); // eslint-disable-line
   core.setFailed(error.message);

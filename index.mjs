e422aae379490a172e543ab3e550990d245d5d49
Morgan Wigmanich <mwigmanich@wistia.com>
2022-04-07 07:47:27 -0400

    refactor to move yet more code from main entrypoint into helpers

--- index.mjs
+++ index.mjs
@@ -1,39 +1,20 @@
-import { execSync } from 'child_process';
 import core from '@actions/core';
 
 import {
-  getInputs,
-  getNpmAuthCommand,
-  getPackageNameAndVersion,
-  getPublishPackageCommand,
   getUniqueVersion,
-  getUpdatePackageVersionCommand,
   loadPackageJson,
   displayInstallationInstructions,
+  publishNpmPackage,
 } from './helpers.mjs';
 
 try {
-  const { npmToken, commitHash, isDryRun } = getInputs();
   const { name, currentVersion } = loadPackageJson();
-  const uniqueVersion = getUniqueVersion(currentVersion, commitHash);
-  const packageNameAndVersion = getPackageNameAndVersion(name, uniqueVersion);
+  const uniqueVersion = getUniqueVersion(currentVersion);
 
-  core.startGroup(`Publish ${name} package to registry`);
+  publishNpmPackage(name, uniqueVersion);
 
-  // set auth token to allow publishing in CI
-  execSync(getNpmAuthCommand(npmToken));
-
-  // update version in package.json (does not get committed)
-  execSync(getUpdatePackageVersionCommand(uniqueVersion));
-
-  // publish package
-  execSync(getPublishPackageCommand(isDryRun));
-
-  core.endGroup();
-
-  // display installation instructions
-  await displayInstallationInstructions(packageNameAndVersion);
+  await displayInstallationInstructions(name, uniqueVersion);
 } catch (error) {
-  console.log(error); // eslint-disable-line no-console
+  core.error(JSON.stringify(error));
   core.setFailed(error.message);
 }

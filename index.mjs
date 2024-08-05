7d5032295dc3507415aeed0fbd148725fac2fdb7
Morgan Wigmanich <mwigmanich@wistia.com>
2024-08-05 18:08:58 -0400

    refactor: move helper code into src directory

--- index.mjs
+++ index.mjs
@@ -1,23 +1,9 @@
-import core from '@actions/core';
-import {
-  displayInstallationInstructions,
-  getUniqueVersion,
-  getWorkingDirectory,
-  loadPackageJson,
-  publishNpmPackage,
-} from './helpers.mjs';
+import { error as displayError, setFailed } from '@actions/core';
+import { publishNpmPackage } from './src/helpers.mjs';
 
 try {
-  // run all subsequent commands in the working directory
-  process.chdir(getWorkingDirectory());
-
-  const { name, currentVersion } = loadPackageJson();
-  const uniqueVersion = getUniqueVersion(currentVersion);
-
-  publishNpmPackage(name, uniqueVersion);
-
-  await displayInstallationInstructions(name, uniqueVersion);
+  await publishNpmPackage();
 } catch (error) {
-  core.error(JSON.stringify(error));
-  core.setFailed(error.message);
+  displayError(JSON.stringify(error));
+  setFailed(error.message);
 }
